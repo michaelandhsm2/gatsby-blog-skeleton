0ca7e907b4421a8513108981c78d5a8ba1ac0296
Michael Fu <michaelandhsm2@gmail.com>
2018-04-04 08:45:36 +0800

    Added Chinese Typography and Basic Layout

---
+++ src/layouts/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Link from "gatsby-link";
+
+const ListLink = props =>
+  <li style={{ display: `inline-block`, marginRight: `1rem` }}>
+    <Link to={props.to}>
+      {props.children}
+    </Link>
+  </li>
+
+export default ({ children }) => (
+  <div style={{ margin: `0 auto`, maxWidth: 650, padding: `1.25rem 1rem` }}>
+    <header style={{ marginBottom: `1.5rem` }}>
+      <Link to="/" style={{ textShadow: `none`, backgroundImage: `none` }}>
+        <h3 style={{ display: `inline` }}>MySweetSite 我的網站</h3>
+      </Link>
+    </header>
+    {children()}
+  </div>
+);
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,27 @@
 import React from "react"
 
-export default () => <div>Hello world!</div>
+export default () => (
+  <div>
+      <h1>Richard Hamming and 老子 on Luck</h1>
+      <div>
+        <p>
+          From Richard <b> Hamming’s 經典著作</b> classic and 必讀 must-read talk,
+          “<a href="http://www.cs.virginia.edu/~robins/YouAndYourResearch.html">
+            You and Your Research
+          </a>”.
+        </p>
+        <blockquote>
+          <p>
+            There is indeed an element of luck (運氣), and no, there isn’t. The prepared
+            mind sooner or later finds something important and does it. So yes, it
+            is luck.{" "}
+            <em>
+              The particular thing you do is luck, but that you do something is
+              not.
+            </em>
+          </p>
+        </blockquote>
+      </div>
+      <p>Posted April 09, 2011</p>
+    </div>
+);
