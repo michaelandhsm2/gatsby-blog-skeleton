900130bf7bfebeec03e180f81dae144f3790cdfd
Michael Fu <michaelandhsm2@gmail.com>
2018-04-20 20:43:45 +0800

    Update Footer Contents

--- src/components/footer.js
+++ src/components/footer.js
@@ -12,10 +12,10 @@ export default class extends React.Component {
     return(
       <Segment inverted vertical textAlign='center'>
         <List horizontal inverted link >
-          <List.Item as='a' href='//www.google.com'><Icon size='large'  name='mail' /></List.Item>
-          <List.Item as='a' href='#'><Icon size='large'  name='facebook f' /></List.Item>
-          <List.Item as='a' href='#'><Icon size='large'  name='twitter' /></List.Item>
-          <List.Item as='a' href='#'><Icon size='large'  name='github' /></List.Item>
+          <List.Item as='a' href='mailto:michaelandhsm2@gmail.com'><Icon size='large'  name='mail' /></List.Item>
+          <List.Item as='a' href='//www.facebook.com/michaelandhsm2'><Icon size='large'  name='facebook f' /></List.Item>
+          <List.Item as='a' href='//twitter.com/Michaelandhsm2'><Icon size='large'  name='twitter' /></List.Item>
+          <List.Item as='a' href='//github.com/michaelandhsm2'><Icon size='large'  name='github' /></List.Item>
         </List>
       </Segment>
     );
